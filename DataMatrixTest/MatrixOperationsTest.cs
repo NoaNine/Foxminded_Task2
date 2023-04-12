7f46db25be58b1097ba35bb8269ec4d091a59e4b
Oleksandr Havlytskyi <havlytskyi@gmail.com>
2023-04-12 13:02:15 +0300

    created tests

--- DataMatrixTest/MatrixOperationsTest.cs
+++ DataMatrixTest/MatrixOperationsTest.cs
@@ -21,10 +21,14 @@ namespace DataMatrixTest
             CollectionAssert.AreEqual(expected, output);
         }
 
-        [TestMethod]
-        public void SecondaryDiagonal()
+        [DataTestMethod]
+        [DataRow(new int[,] { { 1, 2, 3 }, { 8, 7, 6 }, { 4, 3, 2 } }, new int[] { 3, 7, 4 })]
+        [DataRow(new int[,] { { 1, 2, 3, 4, 5 }, { 8, 7, 6, 2, 1 }, { 4, 3, 2, 8, 5 } }, new int[] { 3, 7, 4 })]
+        public void SecondaryDiagonal(int[,] input, int[] expected)
         {
-
+            MatrixOperations matrixTrace = new MatrixOperations();
+            int[] output = matrixTrace.GetMainDiagonal(input);
+            CollectionAssert.AreEqual(expected, output);
         }
 
         [TestMethod]
