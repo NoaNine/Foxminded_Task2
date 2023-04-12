4e970abc3b069a69525ec219c869f2780bd7d2b0
Oleksandr Havlytskyi <havlytskyi@gmail.com>
2023-04-12 14:46:27 +0300

    added algoritm spiral order

--- DataMatrix/MatrixOperations.cs
+++ DataMatrix/MatrixOperations.cs
@@ -1,4 +1,6 @@
-﻿namespace DataMatrix
+﻿using System.Data.Common;
+
+namespace DataMatrix
 {
     public class MatrixOperations
     {
@@ -9,22 +11,79 @@
                 return new int[0];
             }
             int[] elementsInSpiralOrder = new int[matrix.GetLength(0)];
+            int index = 0;
+            int i;
+            int startingColumnIndex = 0;
+            int endingColumnIndex = 0;
+            int startingRowIndex = 0;
+            int endingRowIndex = 0;
+
+            while (startingRowIndex < endingRowIndex && startingColumnIndex < endingColumnIndex)
+            {
+                for (i = startingColumnIndex; i < endingColumnIndex; i++)
+                {
+                    elementsInSpiralOrder[index] = matrix[startingRowIndex, i];
+                }
+                startingRowIndex++;
+
+                for (i = startingRowIndex; i < endingRowIndex; i++)
+                {
+                    elementsInSpiralOrder[index] = matrix[i, endingColumnIndex - 1];
+                }
+                endingColumnIndex--;
+
+                if (startingRowIndex < endingRowIndex)
+                {
+                    for (i = endingColumnIndex - 1; i >= startingColumnIndex; i--)
+                    {
+                        elementsInSpiralOrder[index] = matrix[endingRowIndex - 1, i];
+                    }
+                    endingRowIndex--;
+                }
+
+                if (startingColumnIndex < endingColumnIndex)
+                {
+                    for (i = endingRowIndex - 1; i >= startingRowIndex; i--)
+                    {
+                        elementsInSpiralOrder[index] = matrix[i, startingColumnIndex];
+                    }
+                    startingColumnIndex++;
+                }
+            }
             return elementsInSpiralOrder;
         }
 
         public int[] GetMainDiagonal(int[,] matrix)
         {
-            if (matrix.GetLength(0) < 2 || matrix.GetLength(1) < 2)
+            //if (matrix.GetLength(0) < 2 || matrix.GetLength(1) < 2)
+            //{
+            //    return new int[0];
+            //}
+            //int[] mainDiagonal = new int[matrix.GetLength(0)];
+            //int length = matrix.GetLength(0) <= matrix.GetLength(1) ? matrix.GetLength(0) : matrix.GetLength(1);
+            //for (int i = 0; i < length; i++)
+            //{
+            //    mainDiagonal[i] = matrix[i, i];
+            //}
+            //return mainDiagonal;
+            try
             {
-                return new int[0];
+                if (matrix.GetLength(0) < 2 || matrix.GetLength(1) < 2)
+                {
+                    throw new Exception("Matrix must not be less than 2x2");
+                }
+                int[] mainDiagonal = new int[matrix.GetLength(0)];
+                int length = matrix.GetLength(0) <= matrix.GetLength(1) ? matrix.GetLength(0) : matrix.GetLength(1);
+                for (int i = 0; i < length; i++)
+                {
+                    mainDiagonal[i] = matrix[i, i];
+                }
+                return mainDiagonal;
             }
-            int[] mainDiagonal = new int[matrix.GetLength(0)];
-            int length = matrix.GetLength(0) <= matrix.GetLength(1) ? matrix.GetLength(0) : matrix.GetLength(1);
-            for (int i = 0; i < length; i++)
+            catch (Exception ex)
             {
-                mainDiagonal[i] = matrix[i, i];
+                return ex.Message;
             }
-            return mainDiagonal;
         }
 
         public int[] GetSecondaryDiagonal(int[,] matrix)
