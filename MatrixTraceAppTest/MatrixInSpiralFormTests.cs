8ebc575f6b3da2d3ac729a4ca4c3d2896b6bb1d3
Oleksandr Havlytskyi <havlytskyi@gmail.com>
2023-04-17 15:15:15 +0300

    refactoring

--- MatrixTraceAppTest/MatrixInSpiralFormTests.cs
+++ MatrixTraceAppTest/MatrixInSpiralFormTests.cs
@@ -11,7 +11,7 @@ namespace MatrixTraceAppTest
             int[,] input = new int[,] { { 1, 2, 3, 4 }, { 8, 7, 6, 5 }, { 4, 3, 2, 9, } };
             int[] expected = new int[] { 1, 2, 3, 4, 5, 9, 2, 3, 4, 8, 7, 6 };
             int[] output;
-            //Assert.AreEqual(expected, output);
+            //CollectionAssert.AreEqual(expected, output);
         }
     }
 }
